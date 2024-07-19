9c1cf6892bb94861bfc48257a1d72db61ab0ccd5
sunzhqr <sanzhar_myrzash@mail.ru>
2024-07-19 19:28:32 +0500

    int8, uint8, float64, complex

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-var weekday string = time.Monday.String()
-
-func init() {
-	weekday = time.Now().Weekday().String()
-}
+import "fmt"
 
 func main() {
-	fmt.Printf("Today is %s", weekday)
+	//var negativeInt int = -1
+	//var positiveInt int = 1
+	//
+	//fmt.Printf("int pos: %d, neg: %d\n", negativeInt, positiveInt)
+
+	//var negativeIntB int8 = -128
+	//var positiveIntB int8 = 128 // int8 contains only -128 to 127
+	//
+	//fmt.Printf("int8 pos: %d, neg: %d\n", negativeIntB, positiveIntB)
+
+	//var negativeUint uint8 = -128 // uint8 contains only 0 to 255
+	//var positiveUint uint8 = 128
+	//
+	//fmt.Printf("uint8 pos: %d, neg: %d\n", negativeUint, positiveUint)
+
+	//var valueGloat64 float64 = 128.1234567890
+	//
+	//fmt.Printf("float64 val: %.2f\n", valueGloat64)
+	//fmt.Printf("pointer: %p\n", &valueGloat64)
+
+	valueComplex := 2 + 4i
+
+	fmt.Printf("complex %g\n", valueComplex)
 }
