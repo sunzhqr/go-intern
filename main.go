6dadfcb32f31baa4da434c56abf96f505339e14d
sunzhqr <sanzhar_myrzash@mail.ru>
2024-07-19 20:29:09 +0500

    string and loops

--- main.go
+++ main.go
@@ -3,27 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	//var negativeInt int = -1
-	//var positiveInt int = 1
-	//
-	//fmt.Printf("int pos: %d, neg: %d\n", negativeInt, positiveInt)
+	s := "lorem"
 
-	//var negativeIntB int8 = -128
-	//var positiveIntB int8 = 128 // int8 contains only -128 to 127
-	//
-	//fmt.Printf("int8 pos: %d, neg: %d\n", negativeIntB, positiveIntB)
+	// loop through bytes(uint8)
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%v (%T): %v (%T):\n", i, i, s[i], s[i])
+	}
 
-	//var negativeUint uint8 = -128 // uint8 contains only 0 to 255
-	//var positiveUint uint8 = 128
-	//
-	//fmt.Printf("uint8 pos: %d, neg: %d\n", negativeUint, positiveUint)
+	// loop through runes(int32)
+	for i, r := range s {
+		fmt.Printf("%v (%T): %v (%T):\n", i, i, r, r)
+	}
 
-	//var valueGloat64 float64 = 128.1234567890
-	//
-	//fmt.Printf("float64 val: %.2f\n", valueGloat64)
-	//fmt.Printf("pointer: %p\n", &valueGloat64)
-
-	valueComplex := 2 + 4i
-
-	fmt.Printf("complex %g\n", valueComplex)
+	fmt.Println(s + string('A'+32) + string('0'+9))
 }
