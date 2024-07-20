c27f4eb364369cb0205f6ebe8be59a932497563f
sunzhqr <sanzhar_myrzash@mail.ru>
2024-07-20 19:59:35 +0500

    variadic func

--- main.go
+++ main.go
@@ -2,17 +2,16 @@ package main
 
 import "fmt"
 
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
 func main() {
-	arr := [10]int{9, 8, 7, 6, 5, 4, 3, 2, 1}
-	slc := []int{19, 18, 17, 16, 15, 14, 13, 12, 11, 10}
-	fmt.Println(len(arr), cap(arr))
-	fmt.Printf("array: %T, slice: %T\n", arr, slc)
-	fmt.Printf("array[1]: %d, slice[1]: %d\n", arr[1], slc[1])
-
-	// arr = append(arr, 123)  // wrong
-	slc = append(slc, 321)
-
-	slc = append(slc[len(slc)-1:], slc[:len(slc)-1]...)
-	fmt.Println(slc)
-
+	sum(1, 2, 3)
+	nums := []int{7, 8, 9}
+	sum(nums...)
 }
