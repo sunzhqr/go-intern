6c1cf53b3afca2d90a94191d4ab09bd0bb90c145
sunzhqr <sanzhar_myrzash@mail.ru>
2024-07-20 19:55:39 +0500

    arr and slc

--- main.go
+++ main.go
@@ -3,18 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	i := 5
-	switch {
-	case i < 5:
-		fmt.Println("i is less than 5")
-		fallthrough
-	case i < 10:
-		fmt.Println("i is less than 10")
-		fallthrough
-	case i < 15:
-		fmt.Println("i is less than 15")
-		fallthrough
-	default:
-		fmt.Printf("i is equal to %d\n", i)
-	}
+	arr := [10]int{9, 8, 7, 6, 5, 4, 3, 2, 1}
+	slc := []int{19, 18, 17, 16, 15, 14, 13, 12, 11, 10}
+	fmt.Println(len(arr), cap(arr))
+	fmt.Printf("array: %T, slice: %T\n", arr, slc)
+	fmt.Printf("array[1]: %d, slice[1]: %d\n", arr[1], slc[1])
+
+	// arr = append(arr, 123)  // wrong
+	slc = append(slc, 321)
+
+	slc = append(slc[len(slc)-1:], slc[:len(slc)-1]...)
+	fmt.Println(slc)
+
 }
