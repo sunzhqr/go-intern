4adfa5e2104550fcb8abc2ef85621943b0373dd3
sunzhqr <sanzhar_myrzash@mail.ru>
2024-07-20 20:02:08 +0500

    anonymous func

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import "fmt"
 
-func sum(nums ...int) {
-	fmt.Print(nums, " ")
-	total := 0
-	for _, num := range nums {
-		total += num
-	}
-	fmt.Println(total)
-}
+//	func sum(nums ...int) {
+//		fmt.Print(nums, " ")
+//		total := 0
+//		for _, num := range nums {
+//			total += num
+//		}
+//		fmt.Println(total)
+//	}
 func main() {
-	sum(1, 2, 3)
-	nums := []int{7, 8, 9}
-	sum(nums...)
+	func() {
+		fmt.Print("Hello ")
+	}()
+	world := func() {
+		fmt.Println("World")
+	}
+	world()
 }
