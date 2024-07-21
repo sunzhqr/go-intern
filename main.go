abe9085730d2e6fe1c2b58845706ce96de504624
sunzhqr <sanzhar_myrzash@mail.ru>
2024-07-21 23:20:39 +0500

    wtf with Harry Potter

--- main.go
+++ main.go
@@ -2,38 +2,36 @@ package main
 
 import "fmt"
 
-type Sigma struct {
-	Level   int
-	IsSigma bool
+type Permission struct {
+	Role   string
+	Access bool
 }
 
-func (s *Sigma) GetInfo() {
-	if s.Level == 0 {
-		fmt.Println("either you are Abylay or Tamerlan(((")
-	} else if s.Level > 50 && s.Level < 100 {
-		fmt.Println("You are Zhandos and you are sigma!")
-		s.IsSigma = true
-	} else if s.Level == 100 {
-		s.IsSigma = true
-		fmt.Println("You are Aigali Bratan and you are king of sigmas!")
-	} else {
-		fmt.Println("Idi nahui Nura, peace of shit")
-	}
+type User struct {
+	Name string
 }
 
-type Character struct {
-	Nick string
-	Sigma
-}
-
-func (c *Character) SetLevel(level int) {
-	c.Level = level
-}
 func main() {
-	user1 := Character{Nick: "zhakyndama"}
-	user1.SetLevel(0)
-	user1.GetInfo()
-	user2 := Character{Nick: "aigalisultankul"}
-	user2.SetLevel(100)
-	user2.GetInfo()
+	var users = []User{{"Harry"}, {"Voldemort"}}
+	var hogwarts = make(map[User][]Permission, 2)
+	for key := range users {
+		fmt.Println(users[key])
+	}
+	for key := range users {
+		if _, ok := hogwarts[users[key]]; !ok {
+			hogwarts[users[key]] = make([]Permission, 0, 2)
+		}
+		hogwarts[users[key]] = append(hogwarts[users[key]], Permission{Role: "read_only", Access: true})
+	}
+
+	for _, user := range users {
+		hogwarts[user] = append(hogwarts[user], Permission{Role: "avada_kadavra", Access: true})
+	}
+
+	hogwarts[User{"Harry"}] = append(hogwarts[User{"Harry"}], Permission{"auto_mirror", true})
+
+	for ind, mag := range hogwarts {
+		fmt.Print("mag = ", ind, mag, "->")
+		fmt.Printf("%+v\n", hogwarts[ind])
+	}
 }
